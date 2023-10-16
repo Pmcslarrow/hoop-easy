0967bffbde718d68d2ea2d4e3534894a3a660256
pmcslarrow <pmcslarrow@icloud.com>
2023-10-15 22:53:22 -0700

    Navbar update 1

--- src/views/CreateAccount.jsx
+++ src/views/CreateAccount.jsx
@@ -1,132 +1,159 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from "../config/firebase"
-import { createUserWithEmailAndPassword, signOut, sendEmailVerification } from 'firebase/auth'
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword, signOut, sendEmailVerification } from 'firebase/auth';
 import { handleError } from './ErrorHandler';
-import { db } from '../config/firebase'
-import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import { db } from '../config/firebase';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { motion, AnimatePresence } from 'framer-motion';
 import './create.css';
-
-// This component utilizes Firebase auth to create a new user, ensuring that the email domain is restricted to "willamette.edu".
-// After successfully creating an account, adhering to the specified email format and password requirements, the CreateAccount
-// function component employs setTimeout to display the message "Please verify your email" for a duration of five seconds.
-// Following this, the user is automatically redirected to the login page, where they can attempt to log in once they have verified
-// their email.
+import hoopEasyLogo from '../images/hoop-easy.png';
+import navButtonImg from '../images/269dd16fa1f5ff51accd09e7e1602267.png';
+import { isDOMComponent } from 'react-dom/test-utils';
 
 function CreateAccount({ setAuthenticationStatus }) {
-      const navigate = useNavigate();
-      const [email, setEmail] = useState('');
-      const [password, setPassword] = useState('');
-      const [errorStatus, setError] = useState(false)
-      const [errorMessage, setMessage] = useState('')
-      const userCollectionRef = collection(db, "users");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorStatus, setError] = useState(false);
+  const [errorMessage, setMessage] = useState('');
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    logout();
+    setAuthenticationStatus(false);
+  }, []);
 
 
-      useEffect(() => {
-            logout()
-            setAuthenticationStatus(false)
-      }, [])
 
-      const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            
-            // Adds email, name and timestamp to users table
-            addUserData();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
 
-            // Send email verification
-            await sendEmailVerification(userCredential.user);
-            setMessage("Please verify your email.")
-            setError(true)
+      addUserData();
 
-            // Redirect to login page after 5 seconds
-            setTimeout(() => {
-                  navigate("/");
-            }, 5000);
-            
-        } catch(err) {
-          handleError(setError, setMessage, err);
-        }
-      };
+      await sendEmailVerification(userCredential.user);
+      setMessage("Please verify your email.");
+      setError(true);
 
-      const addUserData= async () => {
-        const currentDate = Timestamp.now();
-        const atIndex = auth?.currentUser?.email?.indexOf('@');
-        const userName = atIndex !== -1 ? auth?.currentUser?.email?.slice(0, atIndex) : ''; 
-      
-        await addDoc(userCollectionRef, {
-          name: userName,
-          email: auth?.currentUser?.email,
-          date: currentDate
-        });
-      };
+      setTimeout(() => {
+        navigate("/");
+      }, 5000);
 
+    } catch(err) {
+      handleError(setError, setMessage, err);
+    }
+  };
 
-      const logout = async () => {
-            try {
-              await signOut(auth)
-            } catch (err) {
-              console.log(err)
-            }
-      }
+  const addUserData = async () => {
+    const currentDate = Timestamp.now();
+    const atIndex = auth?.currentUser?.email?.indexOf('@');
+    const userName = atIndex !== -1 ? auth?.currentUser?.email?.slice(0, atIndex) : '';
 
-      const loginPage = () => {
-            navigate("/")
-      }
+    await addDoc(userCollectionRef, {
+      name: userName,
+      email: auth?.currentUser?.email,
+      date: currentDate
+    });
+  };
 
-      function Header() {
-        return (
-          <header>
-            <div className='flex'>
-              <div className="logo">
-                <img src="your-logo-left.png" alt="Left Logo" />
-              </div>
-              <div className="brand">
-                <span>Hoop:Easy</span>
-              </div>
-            </div>
-            <div className="spacer"></div>
-            <div className="logo">
-              <img src="your-logo-right.png" alt="Right Logo" />
-            </div>
-          </header>
-        );
-      }
-      
-      function LeftPanel() {
-        return (
-          <div className="left-panel">
-            left
-          </div>
-        );
-      }
-      
-      function RightPanel() {
-        return (
-          <div className="right-panel">
-            <h1>Join the fastest growing community in basketball</h1>
-            
-            <div>
-                <div className="rect">
-                  <h1>Create An Account</h1>
-                </div>
-                <p>already have one? login <b className="bold">here</b>.</p>
-            </div>
-          </div>
-        );
-      }
-      
-      
-      return (
-        <div className="App">
-          <Header />
-          <main>
-            <LeftPanel />
-            <RightPanel />
-          </main>
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const loginPage = () => {
+    navigate("/");
+  }
+
+  function LeftPanel() {
+    return (
+      <div className="left-panel">
+        left
+      </div>
+    );
+  }
+
+  function RightPanel() {
+    return (
+      <div className="right-panel">
+        <h1>Join the fastest growing community in basketball</h1>
+
+        <div>
+          <span className="rect">
+            <h1>Create An Account</h1>
+          </span>
+          <p>already have one? login <b className="bold">here</b>.</p>
         </div>
+      </div>
+    );
+  }
+
+  function Header() {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+  
+    const Navbar = () => {
+      return (
+        <header>
+          <img src={hoopEasyLogo} alt="Logo" />
+          <div className="spacer"></div>
+          <div className="logo">
+            <img src={navButtonImg} style={{"width": "50px"}} onClick={toggleSidebar} alt="Navigation button (three lines)" />
+          </div>
+        </header>
       );
+    }
+  
+    const toggleSidebar = () => {
+      setSidebarOpen(!isSidebarOpen);
+    };
+  
+    return (
+      <>
+        <Navbar />
+  
+        <AnimatePresence>
+          {isSidebarOpen && (
+            <motion.div
+              className="sidebar"
+              initial={{ width: '100%', height: '0%', zIndex: 1 }} // Add a higher z-index
+              animate={{ width: '100%', height: '88%', zIndex: 1 }} // Add a higher z-index
+              exit={{
+                width: '100%',
+                height: '0%',
+                transition: { duration: 0.3 },
+              }}
+            >
+              <motion.div
+                initial={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 0.1 } }}
+              >
+                <a href="#" className="sidebar-link">Link 1</a>
+                <a href="#" className="sidebar-link">Link 2</a>
+                <a href="#" className="sidebar-link">Link 3</a>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </>
+    );
+  }
+  
+  
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <LeftPanel />
+        <RightPanel />
+      </main>
+    </div>
+  );
 }
 
 export default CreateAccount;
