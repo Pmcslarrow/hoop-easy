33ef771574954bd84e7ff1c49c809ae192b4cd89
Paul McSlarrow <pmcslarrow@icloud.com>
2024-01-30 10:42:41 -0800

    REACT_APP_ .env

--- hoop-easy-client/src/config/firebase.js
+++ hoop-easy-client/src/config/firebase.js
@@ -3,34 +3,34 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from "firebase/storage";
 const {
-    APIKEY,
-    DOMAIN,
-    PROJECTID,
-    BUCKET,
-    MSI,
-    APPID,
-    MEASUREMENT
+    REACT_APP_APIKEY,
+    REACT_APP_DOMAIN,
+    REACT_APP_PROJECTID,
+    REACT_APP_BUCKET,
+    REACT_APP_MSI,
+    REACT_APP_APPID,
+    REACT_APP_MEASUREMENT
 } = process.env
 
 console.log(
-    "LOGGING THE FIREBASE VARIABLES: ",
-    APIKEY,
-    DOMAIN,
-    PROJECTID,
-    BUCKET,
-    MSI,
-    APPID,
-    MEASUREMENT
+    "LOGGING THE FIREBASE VARIABLES REACT_APP_: ",
+    REACT_APP_APIKEY,
+    REACT_APP_DOMAIN,
+    REACT_APP_PROJECTID,
+    REACT_APP_BUCKET,
+    REACT_APP_MSI,
+    REACT_APP_APPID,
+    REACT_APP_MEASUREMENT
 )
 
 const firebaseConfig = {
-    apiKey: APIKEY,
-    authDomain: DOMAIN,
-    projectId: PROJECTID,
-    storageBucket: BUCKET,
-    messagingSenderId: MSI,
-    appId: APPID,
-    measurementId: MEASUREMENT
+    apiKey: REACT_APP_APIKEY,
+    authDomain: REACT_APP_DOMAIN,
+    projectId: REACT_APP_PROJECTID,
+    storageBucket: REACT_APP_BUCKET,
+    messagingSenderId: REACT_APP_MSI,
+    appId: REACT_APP_APPID,
+    measurementId: REACT_APP_MEASUREMENT
 };
 
 const app = initializeApp(firebaseConfig);
