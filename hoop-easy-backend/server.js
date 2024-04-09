3d65eb339c30865f54167302c97f43f96235de38
Paul McSlarrow <74205136+Pmcslarrow@users.noreply.github.com>
2024-04-09 12:58:59 -0700

    Update server.js
    Did this in github edit. Let's hope this works. 
    
    My hypothesis is that there will be problems with convertToLocalTime

--- hoop-easy-backend/server.js
+++ hoop-easy-backend/server.js
@@ -12,6 +12,80 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// GLOBALS
+function createData(col1, col2, col3, col4, id) {
+  return {  col1, col2, col3, col4, id };
+}
+
+function convertToLocalTime( storedUtcDateTime ) {
+    const userLocalDateTime = new Date(storedUtcDateTime);
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const userDateTimeString = userLocalDateTime.toLocaleString('en-US', { timeZone: userTimeZone });
+    return userDateTimeString
+}
+
+function extractDateTime(datetime) {
+    const split = datetime.split(',')
+    return { date: split[0], time: split[1] }
+}
+
+async function sortGamesByLocationDistance (games) {
+    const userCoordinates = await getUserCoordinates();
+    const { latitude: userLat, longitude: userLon } = userCoordinates;
+    const sortedGames = games.sort((game1, game2) => {
+        const distance1 = getDistanceFromLatLonInMiles(userLat, userLon, game1.latitude, game1.longitude);
+        const distance2 = getDistanceFromLatLonInMiles(userLat, userLon, game2.latitude, game2.longitude);
+        return distance1 - distance2;
+    });
+    
+    sortedGames.forEach((game) => {
+        game.distance = getDistanceFromLatLonInMiles(userLat, userLon, game.latitude, game.longitude);
+        game.time = convertToLocalTime(game.dateOfGameInUTC);
+    });
+    
+    return sortedGames;
+};
+
+export default function getUserCoordinates() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                   resolve({
+                       latitude: position.coords.latitude,
+                       longitude: position.coords.longitude
+                   });
+                },
+                (error) => {
+                   reject(error);
+                }
+            );
+        } else {
+            reject('Geolocation is not supported by this browser.');
+        }
+    });
+ }
+
+function getDistanceFromLatLonInMiles(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2 - lat1); // deg2rad below
+    var dLon = deg2rad(lon2 - lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    var dInMiles = d * 0.621371; // Convert to miles
+    return dInMiles;
+ }
+ 
+ function deg2rad(deg) {
+    return deg * (Math.PI/180)
+ }
+
+
 const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
@@ -42,6 +116,21 @@ app.get('/api/games', (req, res) => {
         res.send(result);
     });
 });
+
+app.get('/api/getRankings', (req, res) => {
+    pool.query('SELECT * FROM users;', (err, result, fields) => {
+        if (err) {
+            res.status(500).json({ message: 'Internal server error' });
+            return;
+        }
+	const rankData = result.data.map((obj, i) => {
+		return createData(obj.username, i + 1, obj.overall, obj.gamesPlayed, obj.id)
+	}).sort((a, b) =>  parseFloat(b.overall) - parseFloat(a.overall));
+        res.send(rankData)
+    });
+});
+
+
 
 app.get('/api/getUser', async (req, res) => {
     try {
@@ -119,7 +208,12 @@ app.get('/api/myGames', async (req, res) => {
             res.status(500).send("Error getting myGames")
             return
         }
-        res.status(200).send(result)
+        const myGames = result.data.map((obj, i) => {
+		const convertedDateTime = convertToLocalTime(obj?.dateOfGameInUTC)
+		const { date, time } = extractDateTime(convertedDateTime)
+		return createData(obj.gameType, obj.address, date, time, obj.gameID)
+	})
+	res.status(200).send(myGames)
     })
 })
 
@@ -130,10 +224,9 @@ app.get('/api/availableGames', async (req, res) => {
             console.error('Error inserting user:', err);
             return res.status(500).json({ message: 'Failed to get available games' });
         }
-        console.log(result)
-        console.log(typeof(result))
-        res.send(result)
-        return result
+	const sortedGames = await sortGamesByLocationDistance(result.data);
+        res.send(sortedGames)
+        return sortedGames
     })
 })
 
